f1f5f2e9b9599903edb22b74cbf265d9469ba671
Sharvesh3 <140963405+Sharvesh3@users.noreply.github.com>
2026-02-06 12:51:09 +0530

    Time: 0 ms (100%), Space: 48.3 MB (46.23%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,39 +1,31 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-                int left = findLeft(nums, target);
-        int right = findRight(nums, target);
-
-        if (left <= right) {
-            return new int[]{left, right};
-        }
-        return new int[]{-1, -1};
+        int first=findBound(nums,target,true);
+        int last=findBound(nums,target,false);
+        return new int[] {first,last};
     }
-    public int findLeft(int[] nums, int target) {
-        int left = 0, right = nums.length - 1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (nums[mid] >= target) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
+    public int findBound(int[] nums,int target, boolean isFirst){
+        int low=0;
+        int high=nums.length-1;
+        int ans=-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target){
+                ans=mid;
+                if(isFirst){
+                    high=mid-1;
+                }
+                else{
+                    low=mid+1;
+                }
+            }
+            else if(nums[mid]>target){
+                high=mid-1;
+            }
+            else {
+                low=mid+1;
             }
         }
-        return left;
-    }
-    public int findRight(int[] nums, int target) {
-        int left = 0, right = nums.length - 1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (nums[mid] <= target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-        return right;
+        return ans;
     }
 }
